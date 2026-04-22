3227a308c1b56a4b04e280897bf1f9007325615f
DanCesRom <123031318+DanCesRom@users.noreply.github.com>
2026-04-21 22:20:38 -0400

    Fiont Change

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router";
 import { resetForDemoIfEnabled } from "./lib/auth";
 import "leaflet/dist/leaflet.css";
-
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import "./index.css";
 
 resetForDemoIfEnabled();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
 );
